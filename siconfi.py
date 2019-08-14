17105f615563cc8b23e5d195e8dd86e6e532d41c
fabiolima-stn <fabio-alexandre.lima@tesouro.gov.br>
2019-08-14 10:36:00 -0300

    Versao inicial com stub da classe Siconfi e método para obter todos os entes

--- siconfi.py
+++ siconfi.py
@@ -7,10 +7,13 @@ Classes de erros da biblioteca.
 class ErroSiconfi(Exception):
     pass
 
-class ErroSiconfiEndpointNaoEncontrado(ErroSiconfi):
+class ErroEndpointNaoEncontrado(ErroSiconfi):
     pass
 
-class ErroSiconfiAcessoEndpoint(ErroSiconfi):
+class ErroServicoIndisponivel(ErroSiconfi):
+    pass
+
+class ErroEndpoint(ErroSiconfi):
     """Erro genérico no acesso ao endpoint."""
 
     def __init__(self, endpoint, resposta_http = None):
@@ -28,16 +31,53 @@ class Siconfi():
 
 
     @classmethod
+    def __executa_request(cls, endpoint):
+        """
+        Executa uma requisição à API.
+
+        Parâmetro:
+            endpoint: endpoint da API a ser requisitado.
+
+        Retorna uma lista de dicionários com a resposta da requisição ou uma 
+        execeção em caso de erro.
+        """
+
+	#TODO: tratar possível paginação, de preferência num método à parte que possa ser reaproveitado
+
+        try:
+            response = requests.get(endpoint)
+        except Exception as erro:
+            raise ErroEndpoint(endpoint)
+
+        if response.status_code != 200:
+            if response.status_code == 404:
+                raise ErroEndpointNaoEncontrado(endpoint)
+            elif response.status_code == 503:
+                raise ErroServicoIndisponivel
+            else:
+                raise ErroEndpoint(endpoint, response.status_code)
+
+        json_data = json.loads(response.text)
+        return json_data["items"]
+
+    @classmethod
     def _obtem_entes(cls):
         """
-        Retorna todos os entes cadastrados no SICONFI.
+        Busca todos os entes cadastrados no SICONFI.
 
-        Retorna um JSON com o código IBGE do ente, seu nome, flag indicativa se é 
-        uma capital de Estado ou não (0 = não, 1 = sim), sigla da região ("BR" no 
-        caso da União), sigla da UF "pai" ("BR" no caso de um Estado e null no caso
-        da União) e esfera ("U" = União, "E" = Estado", "M" = Município).
+        Retorna uma lista com o seguinte dicionário:
+            'cod_ibge': código IBGE do ente
+            'ente': nome do ente
+            'capital': flag indicativa se o ente é uma capital de Estado ou não 
+                (0 = não, 1 = sim)
+            'regiao': sigla da região política do ente
+            	('BR' no caso da União)
+            'uf': sigla da UF "pai" do ente
+            	('BR' no caso de um Estado e None no caso da União) 
+            'esfera': esfera do ente
+            	('U' = União, 'E' = Estado, 'M' = Município).
 
-        Em caso de erro na requisição, dispara uma exceção.
+        Dispara uma execeção em casso de erro.
         """
 
         # só executa o endpoint uma vez
@@ -45,21 +85,8 @@ class Siconfi():
             return cls.__entes
 
         endpoint = "http://apidatalake.tesouro.gov.br/ords/siconfi/tt/entes"
-        try:
-            response = requests.get(endpoint)
-        except Exception as erro:
-            raise ErroSiconfiAcessoEndpoint(endpoint)
 
-        if response.status_code != 200:
-            if response.status_code == 404:
-                raise ErroSiconfiEndpointNaoEncontrado(endpoint)
-
-            else:
-                raise ErroSiconfiAcessoEndpoint(endpoint, response.status_code)
-
-        json_data = json.loads(response.text)
-        cls.__entes = json_data["items"]
-
+        cls.__entes = cls.__executa_request(endpoint)
         return cls.__entes
 
 
@@ -69,11 +96,16 @@ class Siconfi():
         pass
 
 
+# cria uma instância e exporta seus métodos para serem chamados pelo programa
+# que importar este módulo de maneira transparente, como se fossem funções 
+# independentes de uma classe
+_inst = Siconfi()
+# ex.: 
+# func = _inst.func
+obtem_entes = _inst._obtem_entes
+
+
 if __name__ == "__main__":
-    try:
-        Siconfi._obtem_entes()
-    except ErroSiconfi as erro:
-        print(str(erro))
-    except Exception as erro:
-        print(erro)
+    #TODO: complementar
+    pass
 
